40efdaf99ed66151206fc4154664f7f9e47fa3e2
Colleen Halpenny <cp.halpenny@gmail.com>
2019-07-24 05:39:33 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,13 @@
 def does_everything(array)
-  if block_
-  i = 0
-  final = []
-  while i < array.length
-    yield array[i]
-    final << array[i]
-    i += 1
-  end
-  final
+  if block_given?
+    i = 0
+    final = []
+    while i < array.length
+      yield array[i]
+      final << array[i]
+      i += 1
+    end
+    final
+  else
+    puts "Please enter a block"
 end
